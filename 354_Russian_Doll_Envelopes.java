7c28e82b93158642864bedc60eaba24266ea9b36
mohitmeena2107 <145155529+mohitmeena2107@users.noreply.github.com>
2024-10-24 00:13:30 +0530

    Praise Me

--- 354_Russian_Doll_Envelopes.java
+++ 354_Russian_Doll_Envelopes.java
@@ -23,3 +23,43 @@ class Solution {
 
     }
 }
+// Updated Binary searxxh with DP Approach
+class Solution {
+    public int maxEnvelopes(int[][] nums) {
+        Arrays.sort(nums,(a,b)->{
+            if(a[0]==b[0]){
+                return b[1]-a[1];
+            }
+            else{
+                return a[0]-b[0];
+            }
+        });
+        int max = 0;
+        int[] dp = new int[nums.length+1];
+        Arrays.fill(dp,Integer.MAX_VALUE);
+        dp[0] = Integer.MIN_VALUE;
+        for(int i = 0;i<nums.length;i++){
+            int a = nums[i][1];
+            int id = bs(dp,a);
+
+            max = Math.max(max,id);
+            if(dp[id]>=a){
+                dp[id] = a;
+            }
+        }
+        return max;
+    }
+    public  int bs(int[] dp,int a){
+        int start = 1,end = dp.length-1;
+        while(start<=end){
+            int mid = start+(end-start)/2;
+            if(dp[mid]<a){
+                start= mid+1;
+            }
+            else{
+                end = mid-1;
+            }
+        }
+        return start;
+    }
+}
